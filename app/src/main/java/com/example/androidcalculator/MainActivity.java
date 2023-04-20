05d7e28105bc8162f130e8f817abf072f43ca2b8
rfloresg <rubflogar@alu.edu.gva.es>
2023-04-20 21:12:14 +0200

    1.0

--- app/src/main/java/com/example/androidcalculator/MainActivity.java
+++ app/src/main/java/com/example/androidcalculator/MainActivity.java
@@ -4,17 +4,78 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
-    String aux = "0";
-    String aux2= "0";
-
+    private CheckBox checkBox;
+    private RadioGroup operaciones;
+    private Button buttonSuma; private Button buttonResta; private Button buttonMultplicar; private Button buttonDividir;
+    private RadioButton suma; private RadioButton resta; private RadioButton division; private RadioButton multiplicar;
+    double result = 0;
+    String aux = "0",aux2= "0";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkBox = findViewById(R.id.MostrarOpciones);
+        operaciones = findViewById(R.id.operaciones);
+        operaciones.setVisibility(View.GONE);
+
+        buttonSuma = findViewById(R.id.sumar);
+        buttonDividir = findViewById(R.id.dividir);
+        buttonMultplicar = findViewById(R.id.multiplicacion);
+        buttonResta = findViewById(R.id.restar);
+
+        suma = findViewById(R.id.suma);
+        resta = findViewById(R.id.resta);
+        multiplicar = findViewById(R.id.multiplicar);
+        division = findViewById(R.id.division);
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkBox.isChecked()){
+                    operaciones.setVisibility(View.VISIBLE);
+                } else {
+                    operaciones.setVisibility(View.GONE);
+                    buttonSuma.setVisibility(View.VISIBLE);
+                    buttonResta.setVisibility(View.VISIBLE);
+                    buttonMultplicar.setVisibility(View.VISIBLE);
+                    buttonDividir.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        operaciones.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (suma.isChecked()){
+                    buttonSuma.setVisibility(View.INVISIBLE);
+                    buttonResta.setVisibility(View.VISIBLE);
+                    buttonMultplicar.setVisibility(View.VISIBLE);
+                    buttonDividir.setVisibility(View.VISIBLE);
+                } else if (resta.isChecked()){
+                    buttonSuma.setVisibility(View.VISIBLE);
+                    buttonResta.setVisibility(View.INVISIBLE);
+                    buttonMultplicar.setVisibility(View.VISIBLE);
+                    buttonDividir.setVisibility(View.VISIBLE);
+                } else if (multiplicar.isChecked()) {
+                    buttonSuma.setVisibility(View.VISIBLE);
+                    buttonResta.setVisibility(View.VISIBLE);
+                    buttonMultplicar.setVisibility(View.INVISIBLE);
+                    buttonDividir.setVisibility(View.VISIBLE);
+                } else if (division.isChecked()) {
+                    buttonSuma.setVisibility(View.VISIBLE);
+                    buttonResta.setVisibility(View.VISIBLE);
+                    buttonMultplicar.setVisibility(View.VISIBLE);
+                    buttonDividir.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 
     public void buttonClear(View view){
@@ -141,51 +202,39 @@ public class MainActivity extends AppCompatActivity {
         aux += "÷";
         textView.setText(aux);
     }
-    public int sumar(int aux , int aux2){
-        return aux + aux2;
-    }
-    public int restar(int aux, int aux2){
-        return aux-aux2;
-    }
-    public int multiplicar(int aux, int aux2){
-        return aux * aux2;
-    }
-    public int dividir(int aux,int aux2){
-        return aux/aux2;
-    }
     public void buttonEquals(View view) {
         textView = findViewById(R.id.Resultado);
-        int aux2 = 0, aux3 = 0,result = 0;
+        String aux2 = "", aux3 = "";
         char signo = 0;
         int pos = 0;
 
         for (int i = 0; i < aux.length(); i++) {
-            if ((char) aux.charAt(i) == (char) 43 || (char) aux.charAt(i) == (char) 45 || (char) aux.charAt(i) == (char) 120 || (char) aux.charAt(i) == (char) 246) {
+            if ((char) aux.charAt(i) == (char) 43 || (char) aux.charAt(i) == (char) 45 || (char) aux.charAt(i) == (char) 120 || (char) aux.charAt(i) == (char) 247) {
                 pos = i;
                 signo = (char) aux.charAt(i);
             }
         }
 
-            for (int i = 0; i < pos; i++) {
+            for (int i = 0; i < aux.length(); i++) {
                 if (i < pos)
-                    aux2 = Integer.parseInt(String.valueOf(aux.charAt(i)));
+                    aux2 += String.valueOf(aux.charAt(i));
             }
             for (int i = 0; i < aux.length(); i++) {
                 if (i > pos)
-                    aux3 = Integer.parseInt(String.valueOf(aux.charAt(i)));
+                    aux3 += String.valueOf(aux.charAt(i));
 
             }
 
             if (signo == (char) 43) {
-                result = aux2 + aux3;
+                result = Double.parseDouble(aux2) + Double.parseDouble(aux3);
             } else if (signo == (char) 45) {
-                result = aux2 - aux3;
+                result = Double.parseDouble(aux2) - Double.parseDouble(aux3);
             } else if (signo == (char) 120) {
-                result = aux2 * aux3;
-            } else if (signo == (char) 246) {
-                result = aux2 / aux3;
+                result = Double.parseDouble(aux2) * Double.parseDouble(aux3);
+            } else if (signo == (char) 247) {
+                result = Double.parseDouble(aux2) / Double.parseDouble(aux3);
             }
-            textView.setText(result);
+            textView.setText(String.valueOf(result));
         }
 
     }
